be9c5219de8f24d82bba7dcae0fd65598e268aa6
Elijah Zupancic <elijahz@snapskout.com>
2015-11-22 14:00:13 -0800

    Fixed checkstyle issues.

--- java-manta-client/src/main/java/com/joyent/manta/client/MantaHttpHeaders.java
+++ java-manta-client/src/main/java/com/joyent/manta/client/MantaHttpHeaders.java
@@ -7,7 +7,16 @@ import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.message.BasicHeader;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import static com.joyent.http.signature.HttpSignerUtils.X_REQUEST_ID_HEADER;
 
