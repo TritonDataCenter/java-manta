1a0dd56cfffceedd8cadee8c6f957e62a0e78518
Elijah Zupancic <elijahz@snapskout.com>
2015-12-11 10:29:28 -0800

    Finished job builder tests.

--- java-manta-it/src/test/java/com/joyent/manta/client/MantaJobBuilderIT.java
+++ java-manta-it/src/test/java/com/joyent/manta/client/MantaJobBuilderIT.java
@@ -5,7 +5,9 @@ package com.joyent.manta.client;
 
 import com.joyent.manta.client.config.TestConfigContext;
 import com.joyent.manta.config.ConfigContext;
+import com.joyent.manta.exception.MantaClientHttpResponseException;
 import com.joyent.manta.exception.MantaCryptoException;
+import com.joyent.manta.exception.MantaErrorCode;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -23,8 +25,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
+ * Tests the execution of Manta compute jobs using the builder fluent interface.
+ *
  * @author <a href="https://github.com/dekobon">Elijah Zupancic</a>
  */
+@Test(dependsOnGroups = "job")
 public class MantaJobBuilderIT {
     private static final String TEST_DATA =
               "line 01 aa\n"
@@ -119,7 +124,45 @@ public class MantaJobBuilderIT {
     }
 
     @Test
-    public void canRerunOldJob() {
+    public void cantRerunOldJob() throws IOException {
+        final MantaJobBuilder builder = mantaClient.jobBuilder();
 
+        String jobName = String.format("job_%s", UUID.randomUUID());
+
+        String path1 = String.format("%s/%s", testPathPrefix, UUID.randomUUID());
+        String path2 = String.format("%s/%s", testPathPrefix, UUID.randomUUID());
+        String path3 = String.format("%s/%s", testPathPrefix, UUID.randomUUID());
+
+        mantaClient.put(path1, TEST_DATA);
+        mantaClient.put(path2, TEST_DATA);
+        mantaClient.put(path3, TEST_DATA);
+
+        MantaJobBuilder.Done finishedJob = builder.newJob(jobName)
+                .addPhase(new MantaJobPhase()
+                        .setExec("grep bb")
+                        .setType("map"))
+                .addPhase(new MantaJobPhase()
+                        .setExec("sort | uniq")
+                        .setType("reduce"))
+                .addInputs(path1, path2, path3)
+                .validateInputs()
+                .run()
+                .waitUntilDone();
+
+        finishedJob.validateJobsSucceeded();
+
+        UUID jobId = finishedJob.getJob().getId();
+
+        boolean failed = false;
+
+        try {
+            mantaClient.endJobInput(jobId);
+        } catch (MantaClientHttpResponseException e) {
+            if (e.getServerCode().equals(MantaErrorCode.INVALID_JOB_STATE_ERROR)) {
+                failed = true;
+            }
+        }
+
+        Assert.assertTrue(failed, "Rerunning completed job worked for some reason");
     }
 }
