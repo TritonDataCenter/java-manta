348838c3ebffedb6b8393ac8b69df75e22e15764
Elijah Zupancic <elijah@zupancic.name>
2016-12-06 10:11:57 -0800

    Added missing maxConnections setter.

--- java-manta-client/src/main/java/com/joyent/manta/config/BaseChainedConfigContext.java
+++ java-manta-client/src/main/java/com/joyent/manta/config/BaseChainedConfigContext.java
@@ -464,6 +464,18 @@ public abstract class BaseChainedConfigContext implements ConfigContext {
         return this;
     }
 
+    /**
+     * Sets the maximum number of open connections to the Manta API.
+     *
+     * @param maxConnections maximum number of open connections to open
+     * @return the current instance of {@link BaseChainedConfigContext}
+     */
+    public BaseChainedConfigContext setMaxConnections(final Integer maxConnections) {
+        this.maxConnections = maxConnections;
+
+        return this;
+    }
+
     @Override
     public boolean equals(final Object other) {
         if (this == other) {
