3565c8af16c847141d285cd81e0474a6b13f5d50
Elijah Zupancic <elijah@zupancic.name>
2016-12-27 10:11:18 -0800

    Adding missing package-info file.

---
+++ java-manta-client/src/main/java/com/joyent/manta/client/jobs/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016, Joyent, Inc. All rights reserved.
+ */
+
+/**
+ * Package containing all classes used for interacting with Manta jobs.
+ *
+ * @author <a href="https://github.com/dekobon">Elijah Zupancic</a>
+ * @since 3.0.0
+ */
+package com.joyent.manta.client.jobs;
