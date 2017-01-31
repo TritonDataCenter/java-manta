b59b7827d205d169b60596ede69111946cd47060
geek <wpreul@gmail.com>
2017-01-31 14:46:15 -0600

    Client-side encryption examples

---
+++ java-manta-examples/src/main/java/ClientEncryptionRangeDownload.java
@@ -0,0 +1,59 @@
+import com.joyent.manta.client.MantaClient;
+import com.joyent.manta.client.MantaObjectResponse;
+import com.joyent.manta.config.*;
+import com.joyent.manta.http.MantaHttpHeaders;
+import sun.misc.BASE64Decoder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+* Usage: set the mantaUserName, privateKeyPath, and publicKeyId with your own values.
+ */
+
+public class ClientEncryptionRangeDownload {
+
+    public static void main(String... args) throws IOException {
+        String mantaUserName = "USERNAME";
+        String privateKeyPath = "PATH/.ssh/id_rsa";
+        String publicKeyId = "04:92:7b:23:bc:08:4f:d7:3b:5a:38:9e:4a:17:2e:df";
+
+        ConfigContext config = new ChainedConfigContext(
+                new DefaultsConfigContext(),
+                new EnvVarConfigContext(),
+                new MapConfigContext(System.getProperties()))
+                .setMantaURL("https://us-east.manta.joyent.com")
+                // If there is no subuser, then just use the account name
+                .setMantaUser(mantaUserName)
+                .setMantaKeyPath(privateKeyPath)
+                .setMantaKeyId(publicKeyId)
+                .setClientEncryptionEnabled(true)
+                .setEncryptionAlgorithm("AES256/CTR/NoPadding")
+                .setEncryptionAuthenticationMode(EncryptionAuthenticationMode.Optional)
+                .setPermitUnencryptedDownloads(false)
+                .setEncryptionKeyId("simple/example")
+                .setEncryptionPrivateKeyBytes((new BASE64Decoder()).decodeBuffer("RkZGRkZGRkJEOTY3ODNDNkM5MUUyMjIyMTExMTIyMjI="));
+
+        try (MantaClient client = new MantaClient(config)) {
+            String mantaPath = "/" + mantaUserName + "/stor/foo";
+
+            MantaObjectResponse response = client.put(mantaPath, "This is my secret\nthat I want encrypted.");
+
+            System.out.println("HTTP Response headers:");
+            System.out.println(response.getHttpHeaders().toString());
+
+            MantaHttpHeaders headers = new MantaHttpHeaders();
+            headers.setByteRange(5L, null);
+
+            // Print out every line from file streamed real-time from Manta
+            try (InputStream is = client.getAsInputStream(mantaPath, headers);
+                 Scanner scanner = new Scanner(is)) {
+
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+            }
+        }
+    }
+}
