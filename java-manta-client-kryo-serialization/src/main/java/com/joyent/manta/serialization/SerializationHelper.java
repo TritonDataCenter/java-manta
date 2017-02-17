364cc7e38835a80fadeba65cd56212c1b8fd6942
Elijah Zupancic <elijah@zupancic.name>
2017-02-16 21:17:33 -0800

    Fixed style issue.

--- java-manta-client-kryo-serialization/src/main/java/com/joyent/manta/serialization/SerializationHelper.java
+++ java-manta-client-kryo-serialization/src/main/java/com/joyent/manta/serialization/SerializationHelper.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.function.Supplier;
