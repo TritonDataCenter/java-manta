04c8c9f0294bcaf7ad85ce8836413827893ee120
Tomas Celaya <tjcelaya@gmail.com>
2017-06-15 11:12:59 +0900

    Resolve intermittent getAsInputStream close issue
    * Resolve getAsInputStream close issue
    
    * put things back in order and only read this.hmac.getMacSize() bytes
    
    * put the HMAC debug log back where it was
    
    * comment about retry bytes check and retry bytes addition check
    
    * Revert CTR-only fixes
    
    * Fixes #250 and adds unit tests.
    
    * Rename context value names to conform to other names
    
    * Commit failing test cases
    
    * Rename hmacBytes context values and add expected size since multiple digest sizes are possible
    
    * Update CHANGELOG for #254 and #250

--- java-manta-client/src/main/java/com/joyent/manta/client/crypto/MantaEncryptedObjectInputStream.java
+++ java-manta-client/src/main/java/com/joyent/manta/client/crypto/MantaEncryptedObjectInputStream.java
@@ -652,6 +652,50 @@ public class MantaEncryptedObjectInputStream extends MantaObjectInputStream {
         return bufferSize;
     }
 
+    /**
+     * Reads the HMAC from the end of the underlying stream and returns it as
+     * a byte array.
+     *
+     * @return HMAC as byte array
+     * @throws MantaIOException thrown when stream is unavailable or invalid
+     */
+    private byte[] readHmacFromEndOfStream() throws MantaIOException {
+        final InputStream stream = super.getBackingStream();
+        final int hmacSize = this.hmac.getMacSize();
+        final byte[] hmacBytes = new byte[hmacSize];
+
+        int totalBytesRead = 0;
+        int bytesRead;
+
+        while (totalBytesRead < hmacSize) {
+            final int lengthToRead = hmacSize - totalBytesRead;
+
+            try {
+                 bytesRead = stream.read(hmacBytes, totalBytesRead, lengthToRead);
+            } catch (IOException e) {
+                String msg = "Unable to read HMAC from the end of stream";
+                MantaIOException mioe = new MantaIOException(msg);
+                annotateException(mioe);
+                mioe.setContextValue("backingStreamClass", stream.getClass());
+                mioe.setContextValue("hmacBytesReadTotal", totalBytesRead);
+                mioe.setContextValue("hmacBytesExpected", hmacSize);
+
+                throw mioe;
+            }
+
+            if (totalBytesRead < hmacSize && bytesRead == EOF) {
+                String msg = "No HMAC was stored at the end of the stream";
+                MantaIOException e = new MantaIOException(msg);
+                annotateException(e);
+                throw e;
+            }
+
+            totalBytesRead += bytesRead;
+        }
+
+        return hmacBytes;
+    }
+
     @Override
     public void close() throws IOException {
         synchronized (this.closeLock) {
@@ -664,32 +708,19 @@ public class MantaEncryptedObjectInputStream extends MantaObjectInputStream {
 
         readRemainingBytes();
 
-        IOUtils.closeQuietly(cipherInputStream);
+        try {
+            IOUtils.closeQuietly(cipherInputStream);
+        } catch (Exception e) {
+            LOGGER.warn("Error closing CipherInputStream", e);
+        }
 
         if (hmac != null && authenticateCiphertext) {
             byte[] checksum = new byte[hmac.getMacSize()];
             hmac.doFinal(checksum, 0);
-            byte[] expected = new byte[this.hmac.getMacSize()];
-            int readHmacBytes = super.getBackingStream().read(expected);
-
-            if (super.getBackingStream().read() != EOF) {
-                String msg = "Expecting the end of the stream. However, more "
-                        + "bytes were available.";
-                MantaIOException e = new MantaIOException(msg);
-                annotateException(e);
-                throw e;
-            }
+            byte[] expected = readHmacFromEndOfStream();
 
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("Calculated HMAC is: {}", Hex.encodeHexString(checksum));
-            }
-
-            if (readHmacBytes != this.hmac.getMacSize()) {
-                MantaIOException e = new MantaIOException("The HMAC stored was in the incorrect size");
-                annotateException(e);
-                e.setContextValue("expectedHmacSize", this.hmac.getMacSize());
-                e.setContextValue("actualHmacSize", readHmacBytes);
-                throw e;
             }
 
             if (super.getBackingStream().read() >= 0) {
