d855bdf7e469d5a1b04f809d9307c4d7aa26a972
Elijah Zupancic <elijah@zupancic.name>
2017-06-21 13:06:26 -0700

    Reverted Manta version file that shouldn't have been checked in.

--- java-manta-client/src/main/java/com/joyent/manta/util/MantaVersion.java
+++ java-manta-client/src/main/java/com/joyent/manta/util/MantaVersion.java
@@ -22,7 +22,7 @@ public final class MantaVersion {
     /**
      * Release version of the SDK.
      */
-    public static final String VERSION = "3.1.2-SNAPSHOT";
+    public static final String VERSION = "3.0.0-SNAPSHOT";
 
     /**
      * Minimum version of client-side encryption supported.
@@ -37,5 +37,5 @@ public final class MantaVersion {
     /**
      * Release date of the SDK.
      */
-    public static final Instant DATE = Instant.parse("2017-06-21T16:40:40Z");
+    public static final Instant DATE = Instant.now();
 }
