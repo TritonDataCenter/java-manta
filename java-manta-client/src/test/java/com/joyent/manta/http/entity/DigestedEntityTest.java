74095e3cbc5bae37dd488c159578aab480721f59
Tomas Celaya <tjcelaya@gmail.com>
2017-06-29 12:15:57 -0700

    Style fixes for DigestedEntityTest

--- java-manta-client/src/test/java/com/joyent/manta/http/entity/DigestedEntityTest.java
+++ java-manta-client/src/test/java/com/joyent/manta/http/entity/DigestedEntityTest.java
@@ -1,7 +1,13 @@
+/*
+ * Copyright (c) 2017, Joyent, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.joyent.manta.http.entity;
 
 import com.twmacinta.util.FastMD5Digest;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,10 +16,6 @@ import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-
-/**
- * Created by tomascelaya on 6/29/17.
- */
 @Test
 public class DigestedEntityTest {
 
@@ -35,7 +37,6 @@ public class DigestedEntityTest {
         Assert.assertTrue(
                 Arrays.equals(initialDigest, retryDigest),
                 "Reuse of DigestedEntity produced differing digest for first retry");
-
 
         // connection reset again
         out.reset();
