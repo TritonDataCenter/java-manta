1602f48719ba43ca2f0a74dac3d9632eca411ff0
Tomas Celaya <tjcelaya@gmail.com>
2017-07-14 13:09:38 -0700

    stash potential deadend

--- java-manta-client/src/main/java/com/joyent/manta/util/CipherCloner.java
+++ java-manta-client/src/main/java/com/joyent/manta/util/CipherCloner.java
@@ -9,12 +9,17 @@ package com.joyent.manta.util;
 
 import com.joyent.manta.exception.MantaReflectionException;
 import com.rits.cloning.Cloner;
+import org.apache.commons.lang3.NotImplementedException;
+import sun.misc.Unsafe;
+import sun.security.pkcs11.wrapper.PKCS11;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherSpi;
 import javax.crypto.NoSuchPaddingException;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.security.NoSuchAlgorithmException;
 
 import static org.apache.commons.lang3.reflect.FieldUtils.getField;
@@ -70,9 +75,52 @@ public final class CipherCloner extends AbstractCloner<Cipher> {
             cloneField(transformsField, source, target);
             cloneField(firstSpiField, source, target);
 
-            writeField(spiField, target, new Cloner().deepClone(readField(spiField, source)));
+            CipherSpi sourceSpi = (CipherSpi) readField(spiField, source);
+
+            if (sourceSpi.getClass().getCanonicalName().equals("sun.security.pkcs11.P11Cipher")) {
+                writeField(spiField, target, p11Clone(sourceSpi));
+            } else {
+                writeField(spiField, target, new Cloner().deepClone(sourceSpi));
+            }
         } catch (Exception e) {
             throw new MantaReflectionException(e);
         }
+    }
+
+    private CipherSpi p11Clone(CipherSpi spi) throws
+            NoSuchMethodException,
+            IllegalAccessException,
+            InstantiationException,
+            InvocationTargetException,
+            ClassNotFoundException {
+        final Class<? extends CipherSpi> p11cipherClass = spi.getClass();
+        final Field tokenField = getField(p11cipherClass, "token", true);
+        final Object token = readField(tokenField, spi, true);
+
+        final Class<?> tokenClass = Class.forName("sun.security.pkcs11.Token");
+        final Field p11Field = getField(tokenClass, "p11", true);
+        final Object p11 = readField(p11Field, token, true);
+
+        final Class<?> p11class = Class.forName("sun.security.pkcs11.wrapper.PKCS11");
+        final Field pNativeDataField = getField(p11class, "pNativeData", true);
+        final long nativeDataPtr = (long) readField(pNativeDataField, p11, true);
+
+
+        final Unsafe unsafe;
+        final Constructor<Unsafe> unsafeConstructor;
+        unsafeConstructor = Unsafe.class.getDeclaredConstructor();
+        unsafeConstructor.setAccessible(true);
+        unsafe = unsafeConstructor.newInstance();
+
+        long ptr = unsafe.allocateMemory(1024L);
+        unsafe.setMemory(ptr, 1024L, (byte) 0);
+
+        unsafe.copyMemory(nativeDataPtr, ptr, 512L);
+
+        final Object newP11 = unsafe.allocateInstance(p11class);
+
+        if (true) throw new NotImplementedException("is this even possible");
+
+        return spi;
     }
 }
