fbc90e7b2c66c4ff7a58df018abdaa6d7763e4ee
Elijah Zupancic <elijah@zupancic.name>
2017-10-02 17:01:55 -0700

    Added static final constants for JSON field keys.

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/client/MantaObjectConversionFunction.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/client/MantaObjectConversionFunction.java
@@ -27,18 +27,63 @@ import static com.joyent.manta.util.MantaUtils.formatPath;
  */
 public class MantaObjectConversionFunction implements Function<Map<String, Object>, MantaObject> {
     /**
+     * Key for name property in directory listing results.
+     */
+    private static final String NAME_FIELD_KEY = "name";
+
+    /**
+     * Key for modification time property in directory listing results.
+     */
+    private static final String MTIME_FIELD_KEY = "mtime";
+
+    /**
+     * Key for object type (dir / file) property in directory listing results.
+     */
+    private static final String TYPE_FIELD_KEY = "type";
+
+    /**
+     * Key for injecting the path to the object in the results returned from apply().
+     */
+    private static final String PATH_FIELD_KEY = "path";
+
+    /**
+     * Key for content-type property in directory listing results.
+     */
+    private static final String CONTENT_TYPE_FIELD_KEY = "contentType";
+
+    /**
+     * Key for e-tag property in directory listing results.
+     */
+    private static final String ETAG_FIELD_KEY = "etag";
+
+    /**
+     * Key for object size property in directory listing results.
+     */
+    private static final String SIZE_FIELD_KEY = "size";
+
+    /**
+     * Key for durability (number of copies) property in directory listing results.
+     */
+    private static final String DURABILITY_FIELD_KEY = "durability";
+
+    /**
+     * Key for content-md5 property in directory listing results.
+     */
+    private static final String CONTENT_MD5_FIELD_KEY = "contentMD5";
+
+    /**
      * Static instance to be used as a singleton.
      */
     static final MantaObjectConversionFunction INSTANCE = new MantaObjectConversionFunction();
 
     @Override
     public MantaObject apply(final Map<String, Object> item) {
-        String name = Validate.notNull(item.get("name"), "Filename is null").toString();
-        String mtime = Validate.notNull(item.get("mtime"), "Modification time is null").toString();
-        String type = Validate.notNull(item.get("type"), "File type is null").toString();
+        String name = Validate.notNull(item.get(NAME_FIELD_KEY), "Filename is null").toString();
+        String mtime = Validate.notNull(item.get(MTIME_FIELD_KEY), "Modification time is null").toString();
+        String type = Validate.notNull(item.get(TYPE_FIELD_KEY), "File type is null").toString();
 
         String objPath = String.format("%s%s%s",
-                StringUtils.removeEnd(item.get("path").toString(), SEPARATOR),
+                StringUtils.removeEnd(item.get(PATH_FIELD_KEY).toString(), SEPARATOR),
                 SEPARATOR,
                 StringUtils.removeStart(name, SEPARATOR));
         MantaHttpHeaders headers = new MantaHttpHeaders();
@@ -47,24 +92,24 @@ public class MantaObjectConversionFunction implements Function<Map<String, Objec
         /* We look for contentType explicitly because it is being added to Manta
          * in a future version and this property may not be available on all
          * Manta installs for quite some time. */
-        if (item.containsKey("contentType")) {
-            String contentType = Objects.toString(item.get("contentType"), null);
+        if (item.containsKey(CONTENT_TYPE_FIELD_KEY)) {
+            String contentType = Objects.toString(item.get(CONTENT_TYPE_FIELD_KEY), null);
             headers.setContentType(contentType);
         } else if (type.equals(MantaObject.MANTA_OBJECT_TYPE_DIRECTORY)) {
             headers.setContentType(MantaObjectResponse.DIRECTORY_RESPONSE_CONTENT_TYPE);
         }
 
-        if (item.containsKey("etag")) {
-            headers.setETag(Objects.toString(item.get("etag")));
+        if (item.containsKey(ETAG_FIELD_KEY)) {
+            headers.setETag(Objects.toString(item.get(ETAG_FIELD_KEY)));
         }
 
-        if (item.containsKey("size")) {
-            long size = Long.parseLong(Objects.toString(item.get("size")));
+        if (item.containsKey(SIZE_FIELD_KEY)) {
+            long size = Long.parseLong(Objects.toString(item.get(SIZE_FIELD_KEY)));
             headers.setContentLength(size);
         }
 
-        if (item.containsKey("durability")) {
-            String durabilityString = Objects.toString(item.get("durability"));
+        if (item.containsKey(DURABILITY_FIELD_KEY)) {
+            String durabilityString = Objects.toString(item.get(DURABILITY_FIELD_KEY));
             if (durabilityString != null) {
                 int durability = Integer.parseInt(durabilityString);
                 headers.setDurabilityLevel(durability);
@@ -72,8 +117,8 @@ public class MantaObjectConversionFunction implements Function<Map<String, Objec
         }
 
         // This property may not be available on all Manta installs for quite some time
-        if (item.containsKey("contentMD5")) {
-            String contentMD5 = Objects.toString(item.get("contentMD5"), null);
+        if (item.containsKey(CONTENT_MD5_FIELD_KEY)) {
+            String contentMD5 = Objects.toString(item.get(CONTENT_MD5_FIELD_KEY), null);
             headers.setContentMD5(contentMD5);
         }
 
