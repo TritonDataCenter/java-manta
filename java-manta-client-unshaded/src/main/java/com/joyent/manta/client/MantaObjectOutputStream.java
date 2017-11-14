43e4b539bf439796a730e4faaba7d5d3c0ccdfaa
Tomas Celaya <tjcelaya@gmail.com>
2017-11-13 19:08:04 -0800

    Expected fix leads to MD5 checksum errors. Needs further investigation

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/client/MantaObjectOutputStream.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/client/MantaObjectOutputStream.java
@@ -298,6 +298,7 @@ public class MantaObjectOutputStream extends OutputStream {
         Boolean innerIsClosed = isInnerStreamClosed(this.httpContent.getWriter());
         if (innerIsClosed != null && !innerIsClosed) {
             this.httpContent.getWriter().flush();
+            this.httpContent.getWriter().close();
         }
 
         synchronized (this.httpContent) {
