TritonDataCenter/java-manta

commit 5a44b1d0b9e2c65567d82e8f0a7026879780bc01

Lazy-loading configuration for MantaClient (#357)
Resolves #311 by reading as many values directly from the config as possible in addition to introducing a new ConfigContext which will manage derived authentication objects: AuthAwareConfigContext. The new class also provides a reload() method for users to indicate that we should regenerate authentication objects. No guarantees are made about the thread-safety of AuthAwareConfigContext.

java-manta-client-unshaded/src/main/java/com/joyent/manta/http/MantaHttpRequestFactory.java
@@ -7,9 +7,15 @@
  */
 package com.joyent.manta.http;
 
-import com.joyent.manta.config.ConfigContext;
+import com.joyent.manta.config.AuthAwareConfigContext;
 import com.joyent.manta.exception.ConfigurationException;
+import com.joyent.manta.exception.MantaClientException;
 import org.apache.commons.lang3.Validate;
+import org.apache.http.Header;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpMessage;
+import org.apache.http.HttpRequest;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
@@ -17,7 +23,9 @@ import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicHeader;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -28,24 +36,53 @@ import java.util.List;
 public class MantaHttpRequestFactory {
 
     /**
-     * Base URL for requests.
+     * Default HTTP headers to attach to all requests.
+     */
+    private static final Header[] HEADERS = {
+            new BasicHeader(MantaHttpHeaders.ACCEPT_VERSION, "~1.0"),
+            new BasicHeader(HttpHeaders.ACCEPT, "application/json, */*")
+    };
+
+    /**
+     * Interceptor which attaches unique IDs to requests.
+     */
+    private static final RequestIdInterceptor INTERCEPTOR_REQUEST_ID = new RequestIdInterceptor();
+
+    /**
+     * Configuration helper to provide to {@link #authInterceptor} and from which to read a base URL.
+     */
+    private final AuthAwareConfigContext authConfig;
+
+    /**
+     * Interceptor for signing requests than can dynamically react to modifications in the provided configuration.
+     */
+    private final DynamicHttpSignatureRequestInterceptor authInterceptor;
+
+    /**
+     * The base url when {@link AuthAwareConfigContext} is not provided.
      */
     private final String url;
 
     /**
-     * Build a new request factory based on a config's URL.
-     * @param config the config from which to extract a URL
+     * Build a new request factory based on an {@link AuthAwareConfigContext}.
+     *
+     * @param authConfig the config from which to extract base a base URL and to provide to the interceptor
      */
-    public MantaHttpRequestFactory(final ConfigContext config) {
-        this(config.getMantaURL());
+    public MantaHttpRequestFactory(final AuthAwareConfigContext authConfig) {
+        this.authConfig = Validate.notNull(authConfig, "AuthAwareConfigContext must not be null");
+        this.authInterceptor = new DynamicHttpSignatureRequestInterceptor(authConfig);
+        this.url = null;
     }
 
     /**
-     * Create an instance of the request factory based on the provided url.
+     * Create an instance of the request factory based on the provided url and no authentication.
+     *
      * @param url the base url
      */
     public MantaHttpRequestFactory(final String url) {
-        this.url = url;
+        this.authConfig = null;
+        this.authInterceptor = null;
+        this.url = Validate.notNull(url, "URL must not be null");
     }
 
     /**
@@ -54,7 +91,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpDelete delete(final String path) {
-        return new HttpDelete(uriForPath(path));
+        final HttpDelete request = new HttpDelete(uriForPath(path));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -64,7 +103,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpDelete delete(final String path, final List<NameValuePair> params) {
-        return new HttpDelete(uriForPath(path, params));
+        final HttpDelete request = new HttpDelete(uriForPath(path, params));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -73,7 +114,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpGet get(final String path) {
-        return new HttpGet(uriForPath(path));
+        final HttpGet request = new HttpGet(uriForPath(path));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -83,7 +126,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpGet get(final String path, final List<NameValuePair> params) {
-        return new HttpGet(uriForPath(path, params));
+        final HttpGet request = new HttpGet(uriForPath(path, params));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -92,7 +137,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpHead head(final String path) {
-        return new HttpHead(uriForPath(path));
+        final HttpHead request = new HttpHead(uriForPath(path));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -102,7 +149,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpHead head(final String path, final List<NameValuePair> params) {
-        return new HttpHead(uriForPath(path, params));
+        final HttpHead request = new HttpHead(uriForPath(path, params));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -111,7 +160,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpPost post(final String path) {
-        return new HttpPost(uriForPath(path));
+        final HttpPost request = new HttpPost(uriForPath(path));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -121,7 +172,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpPost post(final String path, final List<NameValuePair> params) {
-        return new HttpPost(uriForPath(path, params));
+        final HttpPost request = new HttpPost(uriForPath(path, params));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -130,7 +183,9 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpPut put(final String path) {
-        return new HttpPut(uriForPath(path));
+        final HttpPut request = new HttpPut(uriForPath(path));
+        prepare(request);
+        return request;
     }
 
     /**
@@ -140,7 +195,42 @@ public class MantaHttpRequestFactory {
      * @return instance of configured {@link org.apache.http.client.methods.HttpRequestBase} object.
      */
     public HttpPut put(final String path, final List<NameValuePair> params) {
-        return new HttpPut(uriForPath(path, params));
+        final HttpPut request = new HttpPut(uriForPath(path, params));
+        prepare(request);
+        return request;
+    }
+
+    /**
+     * Apply default headers and interceptors to the new request.
+     *
+     * @param request request object to prepare
+     */
+    private void prepare(final HttpRequest request) {
+        request.setHeaders(HEADERS);
+
+        try {
+            INTERCEPTOR_REQUEST_ID.process(request, null);
+
+            if (authInterceptor != null) {
+                authInterceptor.process(request, null);
+            }
+        } catch (HttpException | IOException e) {
+            throw new MantaClientException("Failed to prepare request", e);
+        }
+    }
+
+    /**
+     * Add headers to an {@link org.apache.http.client.methods.HttpUriRequest} without clobbering defaults
+     * and authentication.
+     *
+     * @param httpMessage request to attach headers to
+     * @param headers headers to attach
+     */
+    public static void addHeaders(final HttpMessage httpMessage, final Header... headers) {
+        Validate.notNull(httpMessage, "HttpMessage must not be null");
+        for (final Header header : headers) {
+            httpMessage.addHeader(header);
+        }
     }
 
     /**
@@ -152,11 +242,21 @@ public class MantaHttpRequestFactory {
     protected String uriForPath(final String path) {
         Validate.notNull(path, "Path must not be null");
 
+        final String format;
         if (path.startsWith("/")) {
-            return String.format("%s%s", this.url, path);
+            format = "%s%s";
         } else {
-            return String.format("%s/%s", this.url, path);
+            format = "%s/%s";
         }
+
+        final String baseURL;
+        if (authConfig != null) {
+            baseURL = authConfig.getMantaURL();
+        } else {
+            baseURL = url;
+        }
+
+        return String.format(format, baseURL, path);
     }
 
     /**

commit 6ee0441d015ced64f30ba16783b8fc9b71e9b3c3

Resolve regression introduced by #357

java-manta-client-unshaded/src/main/java/com/joyent/manta/http/MantaHttpRequestFactory.java
@@ -229,6 +229,10 @@ public class MantaHttpRequestFactory {
     public static void addHeaders(final HttpMessage httpMessage, final Header... headers) {
         Validate.notNull(httpMessage, "HttpMessage must not be null");
         for (final Header header : headers) {
+            if (header.getName().equals(MantaHttpHeaders.REQUEST_ID)) {
+                continue;
+            }
+
             httpMessage.addHeader(header);
         }
     }

java-manta-client-unshaded/src/main/java/com/joyent/manta/http/RequestIdInterceptor.java
@@ -61,6 +61,12 @@ public class RequestIdInterceptor implements HttpRequestInterceptor {
 
     @Override
     public void process(final HttpRequest request, final HttpContext context) throws HttpException, IOException {
+        if (request.containsHeader(MantaHttpHeaders.REQUEST_ID)) {
+            // See: MANTA-3673, manta barfs on malformed x-request-id
+            // TODO: what do? throw exception, log warning/error and skip?
+            return;
+        }
+
         final UUID id = TIME_BASED_GENERATOR.generate();
         final String requestId = id.toString();
         final Header idHeader = new BasicHeader(MantaHttpHeaders.REQUEST_ID, requestId);