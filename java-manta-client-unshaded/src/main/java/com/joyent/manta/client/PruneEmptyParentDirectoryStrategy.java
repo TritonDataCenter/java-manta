806a6ad07d231f0b5c98e960b45b924afc2b84dc
Elijah Zupancic <elijah@zupancic.name>
2018-09-27 15:24:04 -0700

    Incorporated CR feedback for naming and javadocs

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategy.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategy.java
@@ -41,31 +41,27 @@ final class PruneEmptyParentDirectoryStrategy {
      * directory is encountered or until a directory is missing.
      *
      * @param client a reference to the manta client instance for the operations
+     * @param headers Manta headers object to send along with delete requests or null
      * @param path file or directory object to delete
-     * @param limit limit of directories to delete
+     * @param maxDepth limit of the depth of directories to delete
      * @throws IOException thrown when there is a problem deleting from Manta
      */
     static void pruneParentDirectories(final MantaClient client,
             final MantaHttpHeaders headers,
             final String path,
-            final int limit) throws IOException {
-
+            final int maxDepth) throws IOException {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Pruning first object: {}", path);
         }
-        /* The first operation is to delete either a file or a directory
-         * at the lowest point. This operation is the minimum criteria for
-         * success. If this operation doesn't succeed, we consider the
-         * prune operation as a failure. That is why all errors from this
-         * operation bubble up. */
+        /* The first operation is to delete an object at the lowest point. This
+         * operation is the minimum criteria for success. If this operation
+         * doesn't succeed, we consider the prune operation as a failure. That
+         * is why all errors from this operation bubble up. */
         client.delete(path, headers, null);
 
-        /* Listing of all directories within an immutable base path (e.g.
-         * /user/stor) with the base path removed. For example, you would see
-         * values like: /user/stor/dir1, /user/stor/dir1/dir2... */
-        final String[] directories = parseDirectoriesFromPathRightToLeft(path);
+        final String[] directories = extractOrderedWriteableAncestorPaths(path);
 
-        final int actualLimit;
+        final int effectiveMaxDepth;
 
         /* If the limit is higher than the directories specified, it may be a
          * logic error on the part of the user of the SDK, but we have made the
@@ -74,30 +70,30 @@ final class PruneEmptyParentDirectoryStrategy {
          * From a user experience perspective, this makes sense and it allows
          * us to be compatible with the existing behavior in the current
          * release. */
-        if (limit > directories.length) {
+        if (maxDepth > directories.length) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Limit [{}] was set to greater than the number of "
-                        + "directories in path [{}]", limit, directories.length);
+                        + "directories in path [{}]", maxDepth, directories.length);
             }
 
-            actualLimit = directories.length;
+            effectiveMaxDepth = directories.length;
 
         /* If the limit is set to -1 (the prune all parents constant), then
          * we automatically set the limit to the number of prunable
          * directories because it allows the user to not have to think about
          * a specific limit. */
-        } else if (limit == PRUNE_ALL_PARENTS) {
-            actualLimit = directories.length;
-        } else if (limit < PRUNE_ALL_PARENTS) {
+        } else if (maxDepth == PRUNE_ALL_PARENTS) {
+            effectiveMaxDepth = directories.length;
+        } else if (maxDepth < PRUNE_ALL_PARENTS) {
             final String msg = String.format("The prune directory limit "
                     + "specified [%d] is less than the minimum value of -1",
-                    limit);
+                    maxDepth);
             throw new IllegalArgumentException(msg);
         } else {
-            actualLimit = limit;
+            effectiveMaxDepth = maxDepth;
         }
 
-        for (int i = 0; i < actualLimit; i++) {
+        for (int i = 0; i < effectiveMaxDepth; i++) {
             try {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("Pruning directory[{}] at path: {}", i, directories[i]);
@@ -124,15 +120,15 @@ final class PruneEmptyParentDirectoryStrategy {
     }
 
     /**
-     * Method that will product an array of strings representing each directory
-     * in a path ordered from right to left with the deepest object excluded.
+     * Method that will produce an array of strings representing each directory
+     * in a path ordered from right to left with the deepest basename excluded.
      * Additionally, immutable base paths like /user/stor will be excluded
      * from the array of directories.
      *
      * @param path path to extract directories from
      * @return array of directory full paths
      */
-    static String[] parseDirectoriesFromPathRightToLeft(final String path) {
+    static String[] extractOrderedWriteableAncestorPaths(final String path) {
         /* Listing of all directories within an immutable base path (e.g.
          * /user/stor) with the base path removed. For example, you would see
          * values like: /user/stor/dir1, /user/stor/dir1/dir2... */
