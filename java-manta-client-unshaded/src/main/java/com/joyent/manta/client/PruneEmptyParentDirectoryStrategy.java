44682c4a4bfb21024af18989650ab6932b1be102
Elijah Zupancic <elijah@zupancic.name>
2018-09-27 11:40:48 -0700

    Additional unit tests and documented code path

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategy.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategy.java
@@ -7,16 +7,14 @@
  */
 package com.joyent.manta.client;
 
-import java.io.IOException;
-
+import com.joyent.manta.exception.MantaClientHttpResponseException;
+import com.joyent.manta.http.MantaHttpHeaders;
+import com.joyent.manta.util.MantaUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.joyent.manta.exception.MantaClientHttpResponseException;
-import com.joyent.manta.exception.MantaErrorCode;
-import com.joyent.manta.http.MantaHttpHeaders;
-import com.joyent.manta.util.MantaUtils;
+import java.io.IOException;
 
 /**
  * Utility class for recursive directory removal strategies.
@@ -37,34 +35,68 @@ final class PruneEmptyParentDirectoryStrategy {
     }
 
     /**
-     * This will delete empty parent directories up to the root.
+     * Method that will delete the specified path. If the delete of the path is
+     * successful, then it will progressively go from right (deepest) to left
+     * in the path attempting to delete all parent directories until a non-empty
+     * directory is encountered or until a directory is missing.
      *
-     * @param client - A valid client.
-     * @param path - THe complete path that you want to start with.
-     * @param limit - The limit of directories to delete.
-     * @throws IOException - this is thrown from
+     * @param client a reference to the manta client instance for the operations
+     * @param path file or directory object to delete
+     * @param limit limit of directories to delete
+     * @throws IOException thrown when there is a problem deleting from Manta
      */
     static void pruneParentDirectories(final MantaClient client,
             final MantaHttpHeaders headers,
             final String path,
             final int limit) throws IOException {
-        // First thing first, delete the child directory.
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("Pruning first directory: {}", path);
+            LOG.debug("Pruning first object: {}", path);
         }
+        /* The first operation is to delete either a file or a directory
+         * at the lowest point. This operation is the minimum criteria for
+         * success. If this operation doesn't succeed, we consider the
+         * prune operation as a failure. That is why all errors from this
+         * operation bubble up. */
         client.delete(path, headers, null);
 
-        // Generating all parent paths.
-        String[] directories = MantaUtils.writeablePrefixPaths(path);
-        ArrayUtils.reverse(directories);
-        // Removing first element, because we have already deleted it.
-        directories = ArrayUtils.remove(directories, 0);
+        /* Listing of all directories within an immutable base path (e.g.
+         * /user/stor) with the base path removed. For example, you would see
+         * values like: /user/stor/dir1, /user/stor/dir1/dir2... */
+        final String[] directories = parseDirectoriesFromPathRightToLeft(path);
 
-        int actualLimit = limit;
-        if (actualLimit > directories.length || actualLimit == PRUNE_ALL_PARENTS) {
+        final int actualLimit;
+
+        /* If the limit is higher than the directories specified, it may be a
+         * logic error on the part of the user of the SDK, but we have made the
+         * decision to not error and instead process all directories available.
+         *
+         * From a user experience perspective, this makes sense and it allows
+         * us to be compatible with the existing behavior in the current
+         * release. */
+        if (limit > directories.length) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Limit [{}] was set to greater than the number of " +
+                        "directories in path [{}]", limit, directories.length);
+            }
+
             actualLimit = directories.length;
+
+        /* If the limit is set to -1 (the prune all parents constant), then
+         * we automatically set the limit to the number of prunable
+         * directories because it allows the user to not have to think about
+         * a specific limit. */
+        } else if (limit == PRUNE_ALL_PARENTS) {
+            actualLimit = directories.length;
+        } else if (limit < PRUNE_ALL_PARENTS) {
+            final String msg = String.format("The prune directory limit "
+                    + "specified [%d] is less than the minimum value of -1",
+                    limit);
+            throw new IllegalArgumentException(msg);
+        } else {
+            actualLimit = limit;
         }
+
         for (int i = 0; i < actualLimit; i++) {
             try {
                 if (LOG.isDebugEnabled()) {
@@ -73,13 +105,45 @@ final class PruneEmptyParentDirectoryStrategy {
 
                 client.delete(directories[i], headers, null);
             } catch (MantaClientHttpResponseException responseException) {
-                if (responseException.getServerCode().equals(MantaErrorCode.RESOURCE_NOT_FOUND_ERROR)) {
-                    continue;
-                } else if (responseException.getServerCode() != MantaErrorCode.DIRECTORY_NOT_EMPTY_ERROR) {
-                    throw responseException;
+                switch (responseException.getServerCode()) {
+                    /* The directory to be deleted was deleted by someone else
+                     * so we stop pruning. */
+                    case RESOURCE_NOT_FOUND_ERROR:
+                        LOG.debug("Directory not found: {}", directories[i]);
+                        return;
+                    /* The directory to be deleted was not empty so we stop
+                       pruning. */
+                    case DIRECTORY_NOT_EMPTY_ERROR:
+                        LOG.debug("Directory not empty: {}", directories[i]);
+                        return;
+                    default:
+                        throw responseException;
                 }
-
             }
         }
+    }
+
+    /**
+     * Method that will product an array of strings representing each directory
+     * in a path ordered from right to left with the deepest object excluded.
+     * Additionally, immutable base paths like /user/stor will be excluded
+     * from the array of directories.
+     *
+     * @param path path to extract directories from
+     * @return array of directory full paths
+     */
+    static String[] parseDirectoriesFromPathRightToLeft(final String path) {
+        /* Listing of all directories within an immutable base path (e.g.
+         * /user/stor) with the base path removed. For example, you would see
+         * values like: /user/stor/dir1, /user/stor/dir1/dir2... */
+        String[] directories = MantaUtils.writeablePrefixPaths(path);
+
+        // We invert the paths so that we process from the deepest nesting
+        ArrayUtils.reverse(directories);
+
+        // Removing first element, because we have already deleted it
+        directories = ArrayUtils.remove(directories, 0);
+
+        return directories;
     }
 }
--- java-manta-client-unshaded/src/test/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategyTest.java
+++ java-manta-client-unshaded/src/test/java/com/joyent/manta/client/PruneEmptyParentDirectoryStrategyTest.java
@@ -2,21 +2,72 @@ package com.joyent.manta.client;
 
 import com.joyent.manta.config.DefaultsConfigContext;
 import com.joyent.manta.config.TestConfigContext;
+import com.joyent.manta.exception.MantaClientHttpResponseException;
+import com.joyent.manta.exception.MantaErrorCode;
 import com.joyent.manta.http.MantaHttpHeaders;
 import org.apache.commons.lang3.ObjectUtils;
 import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 import static com.joyent.manta.client.PruneEmptyParentDirectoryStrategy.PRUNE_ALL_PARENTS;
+import static com.joyent.manta.client.PruneEmptyParentDirectoryStrategy.parseDirectoriesFromPathRightToLeft;
 import static com.joyent.manta.client.PruneEmptyParentDirectoryStrategy.pruneParentDirectories;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @Test
 public class PruneEmptyParentDirectoryStrategyTest {
+    public void canParseObjectPath() {
+        final String path = "/user/stor/dir1/dir2/dir3/object.txt";
+        final String[] expected = new String[] {
+                "/user/stor/dir1/dir2/dir3",
+                "/user/stor/dir1/dir2",
+                "/user/stor/dir1"
+        };
+        final String[] directories = parseDirectoriesFromPathRightToLeft(path);
+
+        Assert.assertEquals(directories, expected);
+    }
+
+    public void canParseObjectPathWithNotNormalizedSeparators() {
+        final String path = "/user/stor/dir1//dir2///dir3/object.txt";
+        final String[] expected = new String[] {
+                "/user/stor/dir1/dir2/dir3",
+                "/user/stor/dir1/dir2",
+                "/user/stor/dir1"
+        };
+        final String[] directories = parseDirectoriesFromPathRightToLeft(path);
+
+        Assert.assertEquals(directories, expected);
+    }
+
+    public void canParseDirectoryPath() {
+        final String path = "/user/stor/dir1/dir2/dir3/dir4/";
+        final String[] expected = new String[] {
+                "/user/stor/dir1/dir2/dir3",
+                "/user/stor/dir1/dir2",
+                "/user/stor/dir1"
+        };
+        final String[] directories = parseDirectoriesFromPathRightToLeft(path);
+
+        Assert.assertEquals(directories, expected);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void willErrorWithALimitBelowMinusOne() throws IOException {
+        final MantaClient client = new MantaClient(new TestConfigContext());
+        final MantaClient clientSpy = spy(client);
+        final MantaHttpHeaders headers = new MantaHttpHeaders();
+        final String path = "/user/stor/dir1/dir2/object.txt";
+
+        // Prevent the client from actually calling out to a real Manta
+        Mockito.doNothing().when(clientSpy).delete(path, headers, null);
+
+        pruneParentDirectories(clientSpy, headers, path, -2);
+    }
+
     public void canPruneEmptyDirectoriesWithDefaultSetting() throws IOException {
         final MantaClient client = new MantaClient(new TestConfigContext());
         final MantaClient clientSpy = spy(client);
@@ -138,6 +189,70 @@ public class PruneEmptyParentDirectoryStrategyTest {
         Mockito.verify(clientSpy, times(1))
                 .delete("/user/stor/dir1/dir2", headers, null);
         Mockito.verify(clientSpy, times(1))
+                .delete("/user/stor/dir1", headers, null);
+
+        // Verify that is the only call that we made
+        Mockito.verifyNoMoreInteractions(clientSpy);
+    }
+
+    public void willStopPruningWhenDirectoryHasContents() throws IOException {
+        final MantaClient client = new MantaClient(new TestConfigContext());
+        final MantaClient clientSpy = spy(client);
+        final MantaHttpHeaders headers = new MantaHttpHeaders();
+        final String path = "/user/stor/dir1/dir2/dir3/object.txt";
+        final int limit = PRUNE_ALL_PARENTS;
+
+        final MantaClientHttpResponseException exception =
+                new MantaClientHttpResponseException("Unit test fake error")
+                .setServerCode(MantaErrorCode.DIRECTORY_NOT_EMPTY_ERROR);
+
+        // Prevent the client from actually calling out to a real Manta
+        Mockito.doNothing().when(clientSpy).delete(path, headers, null);
+        Mockito.doNothing().when(clientSpy).delete("/user/stor/dir1/dir2/dir3", headers, null);
+        Mockito.doThrow(exception)
+                .when(clientSpy).delete("/user/stor/dir1/dir2", headers, null);
+
+        pruneParentDirectories(clientSpy, headers, path, limit);
+
+        Mockito.verify(clientSpy, times(1))
+                .delete(path, headers, null);
+        Mockito.verify(clientSpy, times(1))
+                .delete("/user/stor/dir1/dir2/dir3", headers, null);
+        Mockito.verify(clientSpy, times(1))
+                .delete("/user/stor/dir1/dir2", headers, null);
+        Mockito.verify(clientSpy, never())
+                .delete("/user/stor/dir1", headers, null);
+
+        // Verify that is the only call that we made
+        Mockito.verifyNoMoreInteractions(clientSpy);
+    }
+
+    public void willStopPruningWhenDirectoryIsNotFound() throws IOException {
+        final MantaClient client = new MantaClient(new TestConfigContext());
+        final MantaClient clientSpy = spy(client);
+        final MantaHttpHeaders headers = new MantaHttpHeaders();
+        final String path = "/user/stor/dir1/dir2/dir3/object.txt";
+        final int limit = PRUNE_ALL_PARENTS;
+
+        final MantaClientHttpResponseException exception =
+                new MantaClientHttpResponseException("Unit test fake error")
+                        .setServerCode(MantaErrorCode.RESOURCE_NOT_FOUND_ERROR);
+
+        // Prevent the client from actually calling out to a real Manta
+        Mockito.doNothing().when(clientSpy).delete(path, headers, null);
+        Mockito.doNothing().when(clientSpy).delete("/user/stor/dir1/dir2/dir3", headers, null);
+        Mockito.doThrow(exception)
+                .when(clientSpy).delete("/user/stor/dir1/dir2", headers, null);
+
+        pruneParentDirectories(clientSpy, headers, path, limit);
+
+        Mockito.verify(clientSpy, times(1))
+                .delete(path, headers, null);
+        Mockito.verify(clientSpy, times(1))
+                .delete("/user/stor/dir1/dir2/dir3", headers, null);
+        Mockito.verify(clientSpy, times(1))
+                .delete("/user/stor/dir1/dir2", headers, null);
+        Mockito.verify(clientSpy, never())
                 .delete("/user/stor/dir1", headers, null);
 
         // Verify that is the only call that we made
