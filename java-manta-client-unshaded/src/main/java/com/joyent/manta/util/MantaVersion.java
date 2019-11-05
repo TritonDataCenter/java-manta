37fca12a2292dcbb80ca129744fbbb87e63df21b
Ashwin Nair <nairashwin952013@gmail.com>
2019-11-05 14:46:21 -0800

    MantaVersion.java updated for new development iteration (#553)

--- java-manta-client-unshaded/src/main/java/com/joyent/manta/util/MantaVersion.java
+++ java-manta-client-unshaded/src/main/java/com/joyent/manta/util/MantaVersion.java
@@ -22,7 +22,7 @@ public final class MantaVersion {
     /**
      * Release version of the SDK.
      */
-    public static final String VERSION = "3.4.1-SNAPSHOT";
+    public static final String VERSION = "3.4.2-SNAPSHOT";
 
     /**
      * Minimum version of client-side encryption supported.
