b1224cd30cce5eb36561dc5a4ef8ba6b0a9fad32
Ashwin A Nair <ashwin.nair@joyent.com>
2019-11-05 16:15:50 -0800

    Missing Copyright Headers

--- java-manta-it/src/test/java/com/joyent/test/util/FailingInputStreamTest.java
+++ java-manta-it/src/test/java/com/joyent/test/util/FailingInputStreamTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015-2019, Joyent, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.joyent.test.util;
 
 import org.testng.Assert;
