TritonDataCenter/java-manta

commit 10e9cbaa01eb75e31ccc0108271d0f57a00251eb

MANTA-5083 MANTA JAVA client is taking too long to get file when range start is big. (#562)

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/AesCtrCipherDetails.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Joyent, Inc. All rights reserved.
+ * Copyright 2020 Joyent, Inc. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,6 +11,11 @@ import com.joyent.manta.client.crypto.AesCipherDetailsFactory.CipherMode;
 import org.apache.commons.lang3.Validate;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+
+import java.math.BigInteger;
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Arrays;
 
 /**
  * Class that provides details about how the AES-CTR cipher's settings.
@@ -120,6 +125,22 @@ public final class AesCtrCipherDetails extends AbstractAesCipherDetails {
         return new ByteRangeConversion(ciphertextStartPositionInclusive, plaintextBytesToSkipInitially,
                 ciphertextEndPositionInclusive, lengthOfPlaintextIncludingSkipBytes, startingBlockNumberInclusive);
     }
+
+    @Override
+    public AlgorithmParameterSpec getEncryptionParameterSpec(final byte[] iv, final long position) {
+        Validate.notNull(iv, "Initialization vector must not be null");
+        Validate.isTrue(iv.length == getIVLengthInBytes(),
+                "Initialization vector has the wrong byte count [%d] "
+                        + "expected [%d] bytes", iv.length, getIVLengthInBytes());
+
+        final int blockSize = getBlockSizeInBytes();
+        final long startingBlock = position / blockSize;
+        final BigInteger ivBigInt = new BigInteger(iv);
+        byte[] updatedIV = Arrays.copyOf(ivBigInt.add(BigInteger.valueOf(startingBlock)).toByteArray(), 16);
+
+        return new IvParameterSpec(updatedIV);
+    }
+
 
     @Override
     public long updateCipherToPosition(final Cipher cipher, final long position) {

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/AbstractAesCipherDetails.java
@@ -217,6 +217,11 @@ public abstract class AbstractAesCipherDetails implements SupportedCipherDetails
 
     @Override
     public AlgorithmParameterSpec getEncryptionParameterSpec(final byte[] iv) {
+        return getEncryptionParameterSpec(iv, 0L);
+    }
+
+    @Override
+    public AlgorithmParameterSpec getEncryptionParameterSpec(final byte[] iv, final long position) {
         Validate.notNull(iv, "Initialization vector must not be null");
         Validate.isTrue(iv.length == getIVLengthInBytes(),
                 "Initialization vector has the wrong byte count [%d] "

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/LocallyIllegalAesCipherDetails.java
@@ -141,6 +141,12 @@ public class LocallyIllegalAesCipherDetails implements SupportedCipherDetails {
     }
 
     @Override
+    public AlgorithmParameterSpec getEncryptionParameterSpec(final byte[] iv, final long counter) {
+        fail();
+        return null;
+    }
+
+    @Override
     public HMac getAuthenticationHmac() {
         fail();
         return null;

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/MantaEncryptedObjectInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Joyent, Inc. All rights reserved.
+ * Copyright 2020 Joyent, Inc. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -177,7 +177,11 @@ public class MantaEncryptedObjectInputStream extends MantaObjectInputStream {
         super(backingStream);
 
         this.authenticateCiphertext = authenticateCiphertext;
-        this.startPosition = startPositionInclusive;
+        if (startPositionInclusive != null) {
+            this.startPosition = startPositionInclusive;
+        } else {
+            this.startPosition = 0L;
+        }
         this.plaintextRangeLength = plaintextRangeLength;
         this.cipherDetails = cipherDetails;
         this.contentLength = super.getContentLength();
@@ -238,11 +242,11 @@ public class MantaEncryptedObjectInputStream extends MantaObjectInputStream {
         final int mode = Cipher.DECRYPT_MODE;
 
         try {
-            this.cipher.init(mode, secretKey, cipherDetails.getEncryptionParameterSpec(iv));
-
-            if (startPosition != null && startPosition > 0) {
-                return cipherDetails.updateCipherToPosition(this.cipher, startPosition);
+            if (startPosition > 0) {
+                this.cipher.init(mode, secretKey, cipherDetails.getEncryptionParameterSpec(iv, startPosition));
+                return startPosition % cipherDetails.getBlockSizeInBytes();
             } else {
+                this.cipher.init(mode, secretKey, cipherDetails.getEncryptionParameterSpec(iv));
                 return 0L;
             }
         } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/SupportedCipherDetails.java
@@ -132,6 +132,17 @@ public interface SupportedCipherDetails {
     AlgorithmParameterSpec getEncryptionParameterSpec(byte[] iv);
 
     /**
+     * Creates the {@link AlgorithmParameterSpec} object needed for the associated
+     * algorithm that is seeded with the passed initialization vector (IV) and counter
+     * value. This variant is only useful for ciphers in counter mode.
+     *
+     * @param iv initialization vector
+     * @param counter counter value indicating counter mode cipher block
+     * @return configured instance
+     */
+    AlgorithmParameterSpec getEncryptionParameterSpec(byte[] iv, long counter);
+
+    /**
      * Creates a new instance of a HMAC calculating class that performs
      * authentication against the ciphertext.
      *

commit 9054e62d63261f072b1b521edbb8ceda19e1b5a1

MANTA-5143 Data integrity issue with Java SDK 3.4.2 (#568)
This corrects a bug introduced in #562 that prevented correct decryption of some objects encrypted using AES CTR mode. The objects susceptible to this bug are ones where IV values generated during the encryption process are interpreted as negative BigInteger values. The calculation of applying the CTR mode offset to the initial, randomly-generated IV and the conversion back to a byte array yielded incorrect results in such cases and prevented correct decryption of the associated objects. This PR changes the method used to determine the IV value to use for decrypting the request to instead use operations directly on the byte array rather than converting the values to BigInteger values.

This PR also adds testing to ensure that the IV calculation when using an offset of zero results in an IV that is identical to the input IV.

It also expands the canRandomlyReadPlaintextPositionFromCiphertext testing to exercise 
both the former and current methods of advancing the Cipher to the requested cipher
block. The testing compares each decryption result to the expected plaintext as
well as compares the results of each method to one another.

java-manta-client-unshaded/src/main/java/com/joyent/manta/client/crypto/AesCtrCipherDetails.java
@@ -13,9 +13,8 @@ import org.apache.commons.lang3.Validate;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 
-import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.security.spec.AlgorithmParameterSpec;
-import java.util.Arrays;
 
 /**
  * Class that provides details about how the AES-CTR cipher's settings.
@@ -133,14 +132,28 @@ public final class AesCtrCipherDetails extends AbstractAesCipherDetails {
                 "Initialization vector has the wrong byte count [%d] "
                         + "expected [%d] bytes", iv.length, getIVLengthInBytes());
 
+        final int ivLength = getIVLengthInBytes();
         final int blockSize = getBlockSizeInBytes();
         final long startingBlock = position / blockSize;
-        final BigInteger ivBigInt = new BigInteger(iv);
-        byte[] updatedIV = Arrays.copyOf(ivBigInt.add(BigInteger.valueOf(startingBlock)).toByteArray(), 16);
 
+        // putLong will only populate 8 bytes of the array so it is important for correctness of
+        // the calculation to start at the offset of 8
+        byte[] startingBlockArray = ByteBuffer.allocate(ivLength).putLong(8, startingBlock).array();
+        byte[] updatedIV = new byte[ivLength];
+        // Loop over the IV starting with the least significant byte to ensure
+        // our carry values are correctly calculated.
+        int carry = 0;
+        for (int ivIndex = ivLength - 1; ivIndex >= 0; ivIndex--) {
+            // Widen the byte types to int types for the arithmetic. The bit
+            // mask is used so that the addition is not performed on negative
+            // values because all Java types are signed.
+            int newIVByteAsInt = ((int)iv[ivIndex] & 0xff) + ((int)startingBlockArray[ivIndex] & 0xff) + carry;
+            carry = newIVByteAsInt >>> 8;
+            byte newIVByte = (byte) newIVByteAsInt;
+            updatedIV[ivIndex] = newIVByte;
+        }
         return new IvParameterSpec(updatedIV);
     }
-
 
     @Override
     public long updateCipherToPosition(final Cipher cipher, final long position) {

java-manta-client-unshaded/src/test/java/com/joyent/manta/client/crypto/AesCtrCipherDetailsTest.java
@@ -16,8 +16,10 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
 
 @Test
@@ -90,8 +92,9 @@ public class AesCtrCipherDetailsTest extends AbstractCipherDetailsTest {
     @Test(groups = {"unlimited-crypto"})
     public void canQueryCiphertextByteRangeAes256() throws Exception {
         SupportedCipherDetails cipherDetails = AesCtrCipherDetails.INSTANCE_256_BIT;
+        SupportedCipherDetails cipherDetails2 = AesCtrCipherDetails.INSTANCE_256_BIT;
         SecretKey secretKey = SecretKeyUtils.generate(cipherDetails);
-        canRandomlyReadPlaintextPositionFromCiphertext(secretKey, cipherDetails);
+        canRandomlyReadPlaintextPositionFromCiphertext(secretKey, cipherDetails, cipherDetails2);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
@@ -131,7 +134,8 @@ public class AesCtrCipherDetailsTest extends AbstractCipherDetailsTest {
     }
 
     protected void canRandomlyReadPlaintextPositionFromCiphertext(final SecretKey secretKey,
-                                                                  final SupportedCipherDetails cipherDetails)
+                                                                  final SupportedCipherDetails cipherDetails,
+                                                                  final SupportedCipherDetails cipherDetails2)
             throws IOException, GeneralSecurityException {
         String text = "A SERGEANT OF THE LAW, wary and wise, " +
                 "That often had y-been at the Parvis, <26> " +
@@ -181,6 +185,9 @@ public class AesCtrCipherDetailsTest extends AbstractCipherDetailsTest {
                 long endCipherTextRange = ranges.getCiphertextEndPositionInclusive();
                 long adjustedPlaintextLength = ranges.getLengthOfPlaintextIncludingSkipBytes();
 
+                // Decrypt using the old method of advancing the Cipher to the correct cipher block.
+                // This method was correct, but had poor performance. It is still useful for testing to
+                // verify the current calculation method is correct.
                 Cipher decryptor = cipherDetails.getCipher();
 
                 decryptor.init(Cipher.DECRYPT_MODE, secretKey, cipherDetails.getEncryptionParameterSpec(iv));
@@ -191,12 +198,61 @@ public class AesCtrCipherDetailsTest extends AbstractCipherDetailsTest {
                 byte[] decrypted = Arrays.copyOfRange(out, (int) adjustedPlaintextRange,
                         (int) Math.min(out.length, adjustedPlaintextLength));
 
+                // Decrypt using the current method of advancing the Cipher to the correct cipher block
+                Cipher decryptor2 = cipherDetails2.getCipher();
+                AlgorithmParameterSpec spec = cipherDetails2.getEncryptionParameterSpec(iv, startCipherTextRange);
+                decryptor2.init(Cipher.DECRYPT_MODE, secretKey, spec);
+
+                byte[] out2 = decryptor2.doFinal(adjustedCipherText);
+                byte[] decrypted2 = Arrays.copyOfRange(out, (int) adjustedPlaintextRange,
+                        (int) Math.min(out2.length, adjustedPlaintextLength));
+
                 String decryptedText = new String(decrypted, StandardCharsets.UTF_8);
+                String decryptedText2 = new String(decrypted2, StandardCharsets.UTF_8);
                 String adjustedText = new String(adjustedPlaintext, StandardCharsets.UTF_8);
 
                 Assert.assertEquals(adjustedText, decryptedText,
                         "Random read output from ciphertext doesn't match expectation " +
                                 "[cipher=" + cipherDetails.getCipherId() + "]");
+                Assert.assertEquals(adjustedText, decryptedText2,
+                        "Random read output from ciphertext doesn't match expectation " +
+                                "[cipher=" + cipherDetails2.getCipherId() + "]");
+                Assert.assertEquals(decryptedText, decryptedText2,
+                        "Decrypted texts using different IV initialization methods do not match " +
+                                "[cipher1=" + cipherDetails.getCipherId() +
+                                " cipher12=" + cipherDetails2.getCipherId() + "]");
+            }
+        }
+    }
+
+    public void verifyIvParameterSpecIdentity() throws Exception {
+        boolean done = false;
+        int iterations = 0;
+        final int maxIterations = 10000;
+        while (!done && iterations++ < maxIterations) {
+            SupportedCipherDetails cipherDetails = AesCtrCipherDetails.INSTANCE_256_BIT;
+            final byte[] iv = cipherDetails.generateIv();
+
+            // Verify that calling getEncryptionParameterSpec with the IV and
+            // a zero offset results in the same IV in the resulting output.
+            // This serves as a regression test for an issue where this property
+            // did not always hold and led to decryption issues. The problem was
+            // specifically related to IV values whose BigInteger representation
+            // was negative and for that reason this test iterates for up to
+            // 10,000 iterations or until an IV whose BigInteger representation
+            // is negative is generated.
+            AlgorithmParameterSpec spec = cipherDetails.getEncryptionParameterSpec(iv, 0L);
+            Cipher cipher = cipherDetails.getCipher();
+            SecretKey secretKey = SecretKeyUtils.generate(cipherDetails);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, spec);
+            Assert.assertEquals(iv, cipher.getIV(), "Calculated initialization " +
+                    "vector does not match expected value [expected=" + Arrays.toString(iv) +
+                    " actual=" + Arrays.toString(cipher.getIV()) + "]");
+
+            // Calculate BigInteger value of IV
+            final BigInteger ivBigInt = new BigInteger(iv);
+            if (ivBigInt.compareTo(BigInteger.ZERO) < 0) {
+                done = true;
             }
         }
     }